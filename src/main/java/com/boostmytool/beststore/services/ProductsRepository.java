a3d7e7e53160eb5b18ea4ae48b6f668d1a628033
Mvnra51 <kasimmvnra@gmail.com>
2024-12-20 09:01:23 +0900

    Restore files from initial commit

--- src/main/java/com/boostmytool/beststore/services/ProductsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.boostmytool.beststore.services;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.boostmytool.beststore.models.Product;
-
-public interface ProductsRepository extends JpaRepository<Product, Integer> {
-
-}
